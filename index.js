e3cceeae8bc6e6d68f1f99458ab53449d2baf30c
Joseph Tarrazi <joseftaerrazi@gmail.com>
2020-11-21 08:35:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,27 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+ if (parseInt(time)<12){
+   return `Good Morning`;
+ } 
+ else if (12<parseInt(time)<17){
+   return `Good Afternoon`;
+ }
+ else if (17<parseInt(time)<24){
+   return `Good Evening`;
+ }
+ else{
+   return;
+ }
+ 
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(TEST){
+ let a= document.getElementById("greeting");
+  a.innerHTML="TEST";
+  return; 
+}
+addEventListener('click', greet);
+console.log(greet());
+addEventListener('click', displayMessage('TEST'));
